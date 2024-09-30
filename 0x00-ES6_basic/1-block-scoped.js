be8d853c934dd9765a624d3112f85748a6bf9044
borisngong <borisngong@gmail>
2024-09-30 21:48:42 +0000

    Block scope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,6 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  let task = trueOrFalse;
-  let task2 = !trueOrFalse;
+  let task = false;
+  let task2 = true;
+
+  if (trueOrFalse) {
+    task = true;
+    task2 = false;
+  }
 
   return [task, task2];
 }
