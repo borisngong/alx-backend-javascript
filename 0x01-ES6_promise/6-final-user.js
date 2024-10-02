66b8c3277dd9ab0cfe4e93cb5e5712d2bf2a0eef
borisngong <borisngong@gmail>
2024-10-02 00:47:23 +0000

    Handle multiple promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,15 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => {
-    return results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
-  });
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]) // Execute both promises
+    .then((res) => (  // Process the results when all promises are settled
+      res.map((o) => ({ // Map over each promise result
+        status: o.status, // Keep the status of the promise ('fulfilled' or 'rejected')
+        value: o.status === 'fulfilled' ? o.value : String(o.reason), // Provide either the value or error reason
+      }))
+    ));
 }
-
