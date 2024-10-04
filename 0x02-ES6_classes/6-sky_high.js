8dcb4c6ab5d29c7ccee396cf98fc35f5fc5e3bd3
borisngong <borisngong@gmail>
2024-10-04 01:02:51 +0000

    Inheritance

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,16 +2,19 @@ import Building from './5-building';
 
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft); // Call the parent constructor to assign the sqft
-    this._floors = floors;
+    super(sqft);
+    this.floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  // Override the evacuationWarningMessage method
+  set floors(value) {
+    this._floors = value;
+  }
+
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    return `Evacuate slowly the ${this.floors} floors`;
   }
 }
