5f51f2f7342ce405c8d34433c8db9543ed793ddf
borisngong <borisngong@gmail>
2024-10-08 17:06:50 -0700

    A function that returns an array of students for a specific city with their new grade

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,3 +1,17 @@
-export default function updateStudentGradeByCity(borod, city, newGrades) {
-  return borod.filter((boro) => boro == city);
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city) // firstly Filter by city
+    .map((student) => {
+      // secomdly Map over filtered students
+      // thirdly the grade for the current student from newGrades
+      const studentGrade = newGrades.find(
+        (grade) => grade.studentId === student.id
+      );
+
+      // finally return the student object with the grade (if found) or 'N/A'
+      return {
+        ...student,
+        grade: studentGrade ? studentGrade.grade : "N/A",
+      };
+    });
 }
