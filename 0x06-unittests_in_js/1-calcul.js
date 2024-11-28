2f76b328ee580d1d2aa120a3a2e238b115683788
borisngong <borisngong@gmail>
2024-11-28 07:21:29 -0800

    Basic test with Mocha and Node assertion library

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -3,17 +3,17 @@ function calculateNumber(type, a, b) {
   const roundedB = Math.round(b);
 
   switch (type) {
-    case "SUM":
+    case 'SUM':
       return roundedA + roundedB;
-    case "SUBTRACT":
+    case 'SUBTRACT':
       return roundedA - roundedB;
-    case "DIVIDE":
+    case 'DIVIDE':
       if (roundedB === 0) {
-        return "Error";
+        return 'Error';
       }
       return roundedA / roundedB;
     default:
-      throw new Error("Invalid type");
+      throw new Error('Invalid type');
   }
 }
 
